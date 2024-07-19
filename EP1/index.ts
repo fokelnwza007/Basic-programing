de70df6d05a3bdd1fe6c97a35c7c4f845b778fb0
fokelnwza007 <fokelnwza007@gmail.com>
2024-07-19 11:37:01 +0700

    การบ้าน

--- EP1/index.ts
+++ EP1/index.ts
@@ -1,1 +1,36 @@
 const fullname: string = 'วัชรพล กอแก้ว'
+
+const a: number = 5
+const b: number = 10
+
+
+console.log(a === b) //เปรีบเทียบเท่ากันไหม
+console.log(a > b) // a > b ไหม
+console.log(a <= b) // a <= b ไหม
+
+
+
+let firstname:string = "watcharapon"
+let y: number = 10
+y = 20 
+let gohome: boolean = true
+
+
+console.log(firstname)
+console.log(y)
+console.log(gohome)
+
+
+var x = 5
+var y = 10
+var hello = 5
+
+var msg = 'hello' // single
+var msg = "can't" // double
+
+var isperson = false
+
+console.log(x)
+console.log(y)
+console.log(isperson)
+console.log(msg)
